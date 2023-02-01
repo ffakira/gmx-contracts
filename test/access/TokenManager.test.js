17a4ae9377f5b3480695d890cefa1aed27c0654a
ffakira <akiraff96@gmail.com>
2023-02-02 01:08:06 +0800

    feat(test): added TokenManager.sol tests

--- test/access/TokenManager.test.js
+++ test/access/TokenManager.test.js
@@ -20,6 +20,12 @@ const ERC721Error = {
   "NotApproved": "ERC721: caller is not token owner or approved"
 }
 
+const GMXTimelockError = {
+  "Forbidden": "GmxTimelock: forbidden",
+  "ActionNotSignalled": "GmxTimelock: action not signalled",
+  "TimeNotPassed": "GmxTimelock: action time not yet passed"
+}
+
 describe("TokenManager.sol", function() {
   beforeEach(async function () {
     [
@@ -438,5 +444,455 @@ describe("TokenManager.sol", function() {
       this.user1.address,
       this.nftId
     )).to.be.revertedWith(ERC721Error.NotApproved)
+  })
+
+  it("signalApproveNFTs", async function() {
+    const nftIds = [21, 22]
+
+    await expect(this.tokenManager.connect(this.user0).signalApproveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      nftIds
+    )).to.be.revertedWith(TokenManagerError.Forbidden)
+
+    await expect(this.tokenManager.connect(this.signer2).signalApproveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      nftIds
+    ))
+  })
+
+  it("signApproveNFTs", async function() {
+    const nftIds = [21, 22]
+
+    await expect(this.tokenManager.connect(this.user0).signApproveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      nftIds,
+      1
+    )).to.be.revertedWith(TokenManagerError.Forbidden)
+
+    await expect(this.tokenManager.connect(this.signer2).signApproveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      nftIds,
+      1
+    )).to.be.revertedWith(TokenManagerError.ActionNotSignalled)
+
+    await this.tokenManager.connect(this.wallet).signalApproveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      nftIds
+    )
+
+    await expect(this.tokenManager.connect(this.user0).signApproveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      nftIds,
+      1
+    )).to.be.revertedWith(TokenManagerError.Forbidden)
+
+    await this.tokenManager.connect(this.signer2).signApproveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      nftIds,
+      1
+    )
+
+    await expect(this.tokenManager.connect(this.signer2).signApproveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      nftIds,
+      1
+    )).to.be.revertedWith(TokenManagerError.AlreadySigned)
+
+    await this.tokenManager.connect(this.signer1).signApproveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      nftIds,
+      1
+    )
+  })
+
+  it("approveNFTs", async function() {
+    const nftIds = [21, 22]
+
+    for (let i = 0; i < 2; i++) {
+      await this.nft0.mint(this.tokenManager.address, nftIds[i])
+    }
+
+    await expect(this.tokenManager.connect(this.wallet).approveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      nftIds,
+      1
+    )).to.be.revertedWith(TokenManagerError.ActionNotSignalled)
+
+    await this.tokenManager.connect(this.wallet).signalApproveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      nftIds
+    )
+
+    await expect(this.tokenManager.connect(this.wallet).approveNFTs(
+      this.nft1.address,
+      this.user2.address,
+      nftIds,
+      1
+    )).to.be.revertedWith(TokenManagerError.ActionNotSignalled)
+
+    await expect(this.tokenManager.connect(this.wallet).approveNFTs(
+      this.nft0.address,
+      this.user0.address,
+      nftIds,
+      1
+    )).to.be.revertedWith(TokenManagerError.ActionNotSignalled)
+
+    await expect(this.tokenManager.connect(this.wallet).approveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      [nftIds[0], nftIds[1] + 1],
+      1
+    )).to.be.revertedWith(TokenManagerError.ActionNotSignalled)
+
+    await expect(this.tokenManager.connect(this.wallet).approveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      nftIds,
+      1
+    )).to.be.revertedWith(TokenManagerError.ActionNotAuthorized)
+
+    await this.tokenManager.connect(this.signer0).signApproveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      nftIds,
+      1
+    )
+
+    await expect(this.tokenManager.connect(this.wallet).approveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      nftIds,
+      1
+    )).to.be.revertedWith(TokenManagerError.InsufficientAuthorization)
+
+    await this.tokenManager.connect(this.signer2).signApproveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      nftIds,
+      1
+    )
+
+    await expect(this.nft0.connect(this.user2).transferFrom(
+      this.tokenManager.address,
+      this.user1.address,
+      nftIds[0]
+    )).to.be.revertedWith(ERC721Error.NotApproved)
+
+    await this.tokenManager.connect(this.wallet).approveNFTs(
+      this.nft0.address,
+      this.user2.address,
+      nftIds,
+      1
+    )
+
+    expect(await this.nft0.balanceOf(this.user1.address)).eq(0)
+    expect(await this.nft0.balanceOf(this.tokenManager.address)).eq(2)
+
+    for (let i = 0; i < 2; i++) {
+      expect(await this.nft0.ownerOf(nftIds[i])).eq(this.tokenManager.address)
+    }
+
+    await this.nft0.connect(this.user2).transferFrom(
+      this.tokenManager.address,
+      this.user1.address,
+      nftIds[0]
+    )
+
+    expect(await this.nft0.balanceOf(this.user1.address)).eq(1)
+    expect(await this.nft0.balanceOf(this.tokenManager.address)).eq(1)
+    expect(await this.nft0.ownerOf(nftIds[0])).eq(this.user1.address)
+    expect(await this.nft0.ownerOf(nftIds[1])).eq(this.tokenManager.address)
+
+    await this.nft0.connect(this.user2).transferFrom(
+      this.tokenManager.address,
+      this.user1.address,
+      nftIds[1]
+    )
+
+    expect(await this.nft0.balanceOf(this.user1.address)).eq(2)
+    expect(await this.nft0.balanceOf(this.tokenManager.address)).eq(0)
+
+    for (let i = 0; i < 2; i++) {
+      expect(await this.nft0.ownerOf(nftIds[i])).eq(this.user1.address)
+    }
+  })
+
+  it("receiveNFTs", async function() {
+    const nftIds = [21, 22]
+
+    for (let i = 0; i < 2; i++) {
+      await this.nft0.mint(this.tokenManager.address, nftIds[i])
+    }
+
+    const TokenManager2 = await ethers.getContractFactory("TokenManager")
+    const tokenManager2 = await TokenManager2.deploy(2)
+
+    await this.tokenManager.connect(this.wallet).signalApproveNFTs(
+      this.nft0.address,
+      tokenManager2.address,
+      nftIds
+    )
+
+    await this.tokenManager.connect(this.signer0).signApproveNFTs(
+      this.nft0.address,
+      tokenManager2.address,
+      nftIds,
+      1
+    )
+
+    await this.tokenManager.connect(this.signer2).signApproveNFTs(
+      this.nft0.address,
+      tokenManager2.address,
+      nftIds,
+      1
+    )
+
+    await expect(tokenManager2.receiveNFTs(
+      this.nft0.address,
+      this.tokenManager.address,
+      nftIds
+    )).to.be.revertedWith(ERC721Error.NotApproved)
+
+    await this.tokenManager.connect(this.wallet).approveNFTs(
+      this.nft0.address,
+      tokenManager2.address,
+      nftIds,
+      1
+    )
+
+    expect(await this.nft0.balanceOf(this.tokenManager.address)).eq(2)
+    expect(await this.nft0.balanceOf(tokenManager2.address)).eq(0)
+    
+    for (let i = 0; i < 2; i++) {
+      expect(await this.nft0.ownerOf(nftIds[i])).eq(this.tokenManager.address)
+    }
+
+    await tokenManager2.receiveNFTs(this.nft0.address, this.tokenManager.address, nftIds)
+
+    expect(await this.nft0.balanceOf(this.tokenManager.address)).eq(0)
+    expect(await this.nft0.balanceOf(tokenManager2.address)).eq(2)
+    
+    for (let i = 0; i < 2; i++) {
+      expect(await this.nft0.ownerOf(nftIds[i])).eq(tokenManager2.address)
+    }
+  })
+
+  it("signalSetAdmin", async function() {
+    await expect(this.tokenManager.connect(this.user0).signalSetAdmin(
+      this.timelock.address, this.user1.address
+    )).to.be.revertedWith(TokenManagerError.Forbidden)
+
+    await expect(this.tokenManager.connect(this.wallet).signalSetAdmin(
+      this.timelock.address, this.user1.address
+    )).to.be.revertedWith(TokenManagerError.Forbidden)
+
+    await this.tokenManager.connect(this.signer0).signalSetAdmin(
+      this.timelock.address, this.user1.address
+    )
+  })
+
+  it("signSetAdmin", async function() {
+    await expect(this.tokenManager.connect(this.user0).signSetAdmin(
+      this.timelock.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.Forbidden)
+
+    await expect(this.tokenManager.connect(this.wallet).signSetAdmin(
+      this.timelock.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.Forbidden)
+
+    await expect(this.tokenManager.connect(this.signer1).signSetAdmin(
+      this.timelock.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.ActionNotSignalled)
+
+    await this.tokenManager.connect(this.signer1).signalSetAdmin(
+      this.timelock.address, this.user1.address
+    )
+
+    await expect(this.tokenManager.connect(this.user0).signSetAdmin(
+      this.timelock.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.Forbidden)
+
+    await expect(this.tokenManager.connect(this.signer1).signSetAdmin(
+      this.timelock.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.AlreadySigned)
+
+    await this.tokenManager.connect(this.signer2).signSetAdmin(
+      this.timelock.address, this.user1.address, 1
+    )
+
+    await expect(this.tokenManager.connect(this.signer2).signSetAdmin(
+      this.timelock.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.AlreadySigned)
+  })
+
+  it("setAdmin", async function() {
+    await expect(this.tokenManager.connect(this.user0).setAdmin(
+      this.timelock.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.Forbidden)
+
+    await expect(this.tokenManager.connect(this.wallet).setAdmin(
+      this.timelock.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.Forbidden)
+
+    await expect(this.tokenManager.connect(this.signer0).setAdmin(
+      this.timelock.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.ActionNotSignalled)
+
+    await this.tokenManager.connect(this.signer0).signalSetAdmin(
+      this.timelock.address, this.user1.address
+    )
+
+    await expect(this.tokenManager.connect(this.signer0).setAdmin(
+      this.user0.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.ActionNotSignalled)
+
+    await expect(this.tokenManager.connect(this.signer0).setAdmin(
+      this.timelock.address, this.user1.address, 2
+    )).to.be.revertedWith(TokenManagerError.ActionNotSignalled)
+
+    await expect(this.tokenManager.connect(this.signer0).setAdmin(
+      this.timelock.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.InsufficientAuthorization)
+
+    await this.tokenManager.connect(this.signer2).signSetAdmin(
+      this.timelock.address, this.user1.address, 1
+    )
+
+    expect(await this.timelock.admin()).eq(this.wallet.address)
+    await this.tokenManager.connect(this.signer2).setAdmin(this.timelock.address, this.user1.address, 1)
+    expect(await this.timelock.admin()).eq(this.user1.address)
+  })
+
+  it("signalSetGov", async function() {
+    await expect(this.tokenManager.connect(this.user0).signalSetGov(
+      this.timelock.address, this.gmx.address, this.user1.address
+    )).to.be.revertedWith(TokenManagerError.Forbidden)
+
+    await this.tokenManager.connect(this.wallet).signalSetGov(
+      this.timelock.address, this.gmx.address, this.user1.address
+    )
+  })
+
+  it("signSetGov", async function() {
+    await expect(this.tokenManager.connect(this.user0).signSetGov(
+      this.timelock.address, this.gmx.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.Forbidden)
+
+    await expect(this.tokenManager.connect(this.signer2).signSetGov(
+      this.timelock.address, this.gmx.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.ActionNotSignalled)
+
+    await this.tokenManager.connect(this.wallet).signalSetGov(
+      this.timelock.address, this.gmx.address, this.user1.address
+    )
+
+    await expect(this.tokenManager.connect(this.user0).signSetGov(
+      this.timelock.address, this.gmx.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.Forbidden)
+
+    await this.tokenManager.connect(this.signer2).signSetGov(
+      this.timelock.address, this.gmx.address, this.user1.address, 1
+    )
+
+    await expect(this.tokenManager.connect(this.signer2).signSetGov(
+      this.timelock.address, this.gmx.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.AlreadySigned)
+
+    await this.tokenManager.connect(this.signer1).signSetGov(
+      this.timelock.address, this.gmx.address, this.user1.address, 1
+    )
+  })
+
+  it("setGov", async function() {
+    await this.gmx.setGov(this.gmxTimelock.address)
+
+    await expect(this.tokenManager.connect(this.user0).setGov(
+      this.gmxTimelock.address, this.gmx.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.Forbidden)
+
+    await expect(this.tokenManager.connect(this.wallet).setGov(
+      this.gmxTimelock.address, this.gmx.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.ActionNotSignalled)
+
+    await this.tokenManager.connect(this.wallet).signalSetGov(
+      this.gmxTimelock.address, this.gmx.address, this.user1.address
+    )
+
+    await expect(this.tokenManager.connect(this.wallet).setGov(
+      this.user2.address, this.gmx.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.ActionNotSignalled)
+
+    await expect(this.tokenManager.connect(this.wallet).setGov(
+      this.gmxTimelock.address, this.user0.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.ActionNotSignalled)
+
+    await expect(this.tokenManager.connect(this.wallet).setGov(
+      this.gmxTimelock.address, this.gmx.address, this.user2.address, 1
+    )).to.be.revertedWith(TokenManagerError.ActionNotSignalled)
+
+    await expect(this.tokenManager.connect(this.wallet).setGov(
+      this.gmxTimelock.address, this.gmx.address, this.user1.address, 2
+    )).to.be.revertedWith(TokenManagerError.ActionNotSignalled)
+
+    await expect(this.tokenManager.connect(this.wallet).setGov(
+      this.gmxTimelock.address, this.gmx.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.ActionNotAuthorized)
+
+    await this.tokenManager.connect(this.signer0).signSetGov(
+      this.gmxTimelock.address, this.gmx.address, this.user1.address, 1
+    )
+
+    await expect(this.tokenManager.connect(this.wallet).setGov(
+      this.gmxTimelock.address, this.gmx.address, this.user1.address, 1
+    )).to.be.revertedWith(TokenManagerError.InsufficientAuthorization)
+
+    await expect(this.gmxTimelock.connect(this.wallet).signalSetGov(
+      this.gmx.address, this.user1.address
+    )).to.be.revertedWith(GMXTimelockError.Forbidden)
+
+    await this.tokenManager.connect(this.signer2).signSetGov(
+      this.gmxTimelock.address, this.gmx.address, this.user1.address, 1
+    )
+
+    await expect(this.gmxTimelock.connect(this.wallet).setGov(
+      this.gmx.address, this.user1.address
+    )).to.be.revertedWith(GMXTimelockError.ActionNotSignalled)
+
+    await this.tokenManager.connect(this.wallet).setGov(
+      this.gmxTimelock.address, this.gmx.address, this.user1.address, 1
+    )
+
+    await expect(this.gmxTimelock.connect(this.wallet).setGov(
+      this.gmx.address, this.user1.address
+    )).to.be.revertedWith(GMXTimelockError.TimeNotPassed)
+
+    await ethers.provider.send("evm_increaseTime", [6 * 24 * 60 * 60 + 10])
+    await ethers.provider.send("evm_mine", []);
+
+    await expect(this.gmxTimelock.connect(this.wallet).setGov(
+      this.gmx.address, this.user1.address
+    )).to.be.revertedWith(GMXTimelockError.TimeNotPassed)
+
+    await ethers.provider.send("evm_increaseTime", [1 * 24 * 60 * 60 * 10])
+    await ethers.provider.send("evm_mine", [])
+
+    expect(await this.gmx.gov()).eq(this.gmxTimelock.address)
+
+    await this.gmxTimelock.connect(this.wallet).setGov(
+      this.gmx.address, this.user1.address
+    )
+
+    expect(await this.gmx.gov()).eq(this.user1.address)
   })
 })
